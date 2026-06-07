984a5ae72f0d3c0efd647a8eca837390453e7bab
Maryam Yaqoob <manuscre0@gmail.com>
2026-06-07 18:34:29 +0000

    Add Chromium for Selenium UI tests

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -8,9 +8,11 @@ BASE_URL = os.getenv("BASE_URL", "http://localhost:5000")
 
 def test_frontend_sentiment():
     chrome_options = Options()
+    chrome_options.binary_location = "/usr/bin/chromium"
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
 
     driver = webdriver.Chrome(options=chrome_options)
 
